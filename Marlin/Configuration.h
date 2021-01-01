c4927e27c657d1d8f057ddccb8bce5703fd88444
Emmanuel WALDNER <emmanuel.waldner@gmail.com>
2021-01-01 20:30:42 +0100

    Validation IF Mixing_Extrudeur

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -734,21 +734,25 @@
  * following movement settings. If fewer factors are given than the
  * total number of extruders, the last value applies to the rest.
  */
-#define DISTINCT_E_FACTORS
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 79.95, 803.82, 403.81}
 
+#if ENABLED(MIXING_EXTRUDER)
 /**
  * Default Axis Steps Per Unit (steps/mm)
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 79.95, 803.82, 403.81 , 410.62}
+  #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 79.95, 803.82, 403.81 , 410.62}
+#define DISTINCT_E_FACTORS
+
+#endif
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 50, 130, 130 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 50, 130}
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -761,7 +765,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 800, 500, 10, 1000, 1000 }
+#define DEFAULT_MAX_ACCELERATION      { 800, 500, 10, 1000 }
 
 #define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
